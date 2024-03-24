10f90ea9b2a1eb857dc5a1457602ca3a6cb0ea2b
Gunnar Scherf <mail@gunnar-scherf.de>
2024-03-24 11:03:22 +0100

    .

--- set-version.sh
+++ set-version.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-VERSION=1.0.12
+VERSION=1.0.13
 APP=server
 
 sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" apps/${APP}/__init__.py
+
+sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" ../afd-wiki/apps/${APP}/__init__.py
+sed -i "s/FROM ghcr\.io\/g10f\/wiki:.*/FROM ghcr\.io\/g10f\/wiki:${VERSION}/" ../afd-wiki/Dockerfile
+
+sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" ../dwbn-wiki/apps/${APP}/__init__.py
+sed -i "s/FROM ghcr\.io\/g10f\/wiki:.*/FROM ghcr\.io\/g10f\/wiki:${VERSION}/" ../dwbn-wiki/Dockerfile
